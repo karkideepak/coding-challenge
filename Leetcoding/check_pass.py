b31ecf84fc8471e7199c78bba49d443da37df85c
karkideepak <hope.deepak2@gmail.com>
2024-02-25 13:19:43 -0600

    check_pass2

--- Leetcoding/check_pass.py
+++ Leetcoding/check_pass.py
@@ -6,34 +6,39 @@ class Authentication(object):
     def __init__(self, username=''):
         self.username = username
     
-    def lower(self):
+    def __lower(self):
         lower = any(c.islower() for c in self.username)
         return lower
     
-    def upper(self):
-        upper = any(c.isUpper() for c in self.username)
+    def __upper(self):
+        upper = any(c.isupper() for c in self.username)
         return upper
     
-    def digit(self):
+    def __digit(self):
         digit = any(c.isdigit() for c in self.username)
         return digit
     
     def validate(self):
-        lower = self.lower()
-        upper = self.upper()
-        digit = self.digit()
+        lower = self.__lower()
+        upper = self.__upper()
+        digit = self.__digit()
 
         length = len(self.username)
         report = lower and upper and digit and length >= 6
 
-    if report:
-        print("Username is strong")
-        retrun True
-        
-    elif not lower:
-        print("Add atleast one lowercase!")
-    elif not digit:
-        print("You didin't use digit")
+        if report:
+            print("Username is strong")
+            return True
+
+        elif not lower:
+            print("Add atleast one lowercase!")
+        elif not digit:
+            print("You didin't use digit")
+        else:
+            pass
+
+C = Authentication("outube1992")
+print(C.validate())
 
     
     
